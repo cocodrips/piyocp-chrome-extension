e1b64402ed2530f712c8bb483bc1761bb8c391a4
cocodrips <cocodrips@gmail.com>
2016-08-06 12:24:25 +0900

    Add markdown style.

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 chrome.tabs.getSelected(null, function (tab) {
     document.getElementById("output").innerText = '"' + tab.title + ' - ' + tab.url + '"';
+    document.getElementById("output_markdown").innerText = '[' + tab.title + '](' + tab.url + ')';
     document.getElementById("title_only").innerText = tab.title;
-    // select all...
 });
-
